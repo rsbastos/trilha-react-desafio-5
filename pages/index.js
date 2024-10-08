7fb0be31ea673def9d97e7f11770e2b46317099b
rsbastos <renedossantosbastos@gmail.com>
2024-10-08 09:52:24 -0300

    finalizado

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Index({ posts, globalData }) {
               key={post.id}
               className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
             >
-              <Link
-                as={`/posts/${post.id}`}
+              <Link  
+                as={`/posts/${post.id}`}             
                 href={`/posts/${post.id}`}
               >
                 <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
@@ -62,7 +62,6 @@ export default function Index({ posts, globalData }) {
 export async function getServerSideProps() {
   const posts = await getPosts();
   const globalData = getGlobalData()
-
-
+ 
   return { props: { posts, globalData } };
 }
--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
+export const api = axios.create({
+  baseURL: 'https://pmucoahxjghajkzvlvhg.supabase.co/rest/v1',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdWNvYWh4amdoYWprenZsdmhnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODIyNDgxNiwiZXhwIjoyMDQzODAwODE2fQ.yVEmraA1vcTWOMsZI85g5pwj1GJpnu7Uno02VRa_slM',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdWNvYWh4amdoYWprenZsdmhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgyMjQ4MTYsImV4cCI6MjA0MzgwMDgxNn0.QpZ0jkKSVFweZQOki-BEKgIt4MXsPlkU1G9NDff5Xio',
+  },
+});
